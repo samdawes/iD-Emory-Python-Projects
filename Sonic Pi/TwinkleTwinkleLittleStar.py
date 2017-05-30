60cc2d46aec471b65b854fad9686707fb978f442
Sam Dawes <sdawes@usc.edu>
2017-05-29 23:59:30 -0400

    adding Sonic Pi directory and a song students could make

---
+++ Sonic Pi/TwinkleTwinkleLittleStar.py
@@ -0,0 +1,62 @@
+#WILL NOT COMPILE WITH NORMAL PYTHON 3
+#COPY/PASTE THE CODE INTO SONIC PI
+
+#IMPORT SONIC PI
+
+#"Twinkle twinkle little star"
+play :C
+
+sleep 0.25
+
+play :C
+
+sleep 0.25
+
+play :G
+
+sleep 0.25
+
+play :G
+
+sleep 0.25
+
+play :A
+
+sleep 0.25
+
+play :A
+
+sleep 0.25
+
+play :G
+
+sleep 1
+
+#"How I wonder what you are"
+play :F
+
+sleep 0.25
+
+play :F
+
+sleep 0.25
+
+play :E
+
+sleep 0.25
+
+play :E
+
+sleep 0.25
+
+play :D
+
+sleep 0.25
+
+play :D
+
+sleep 0.25
+
+play :C
+
+sleep 1
