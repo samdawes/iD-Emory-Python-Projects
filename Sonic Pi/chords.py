5811eb1fae873caf3d5f23e9dc9f9580dddc9e88
Sam Dawes <sdawes@usc.edu>
2017-05-30 00:09:28 -0400

    added file about chords in sonic pi

---
+++ Sonic Pi/chords.py
@@ -0,0 +1,19 @@
+#Copy/Paste this code into Sonic Pi
+
+#C-Major Chord
+play :C
+play :E
+play :G
+
+#C-Minor Chord
+play :C
+play :Eb
+play :G
+
+#C-Augmented Chord
+play :C
+play :Eb
+play :Gs
+
+#G-Major Chord
+play_chord [:G, :B, :D]
