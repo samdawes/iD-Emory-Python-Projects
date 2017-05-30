0f4b223162afc38fc75d1ab480b9f418cd160fd3
Sam Dawes <sdawes@usc.edu>
2017-05-29 23:47:51 -0400

    added file for teaching basic syntax

---
+++ Basic Python Syntax/syntax.py
@@ -0,0 +1,36 @@
+
+######################################
+#Print statements:
+######################################
+
+#String
+print ("Hello world!")
+
+#Addition
+print (2 + 2)
+
+#Subtraction
+print (2 - 2)
+
+#Multiplication
+print (2 * 2)
+
+#Division
+print (2 / 2)
+
+######################################
+#Variables: 4 Data Types
+######################################
+
+#int
+x = 2
+
+#float
+f = 2.25
+
+#boolean
+bool1 = True
+bool2 = False
+
+#String
+s = "hello world"
